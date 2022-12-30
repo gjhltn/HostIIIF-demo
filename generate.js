abd6c47831cec0e2c84d8189d992ea43d41d691b
g j h <guy@swirrl.com>
2022-12-30 10:51:33 +0000

    generate

--- generate.js
+++ generate.js
@@ -1,16 +1,27 @@
-var sharp = require("sharp");
+const fs = require('fs');
+const sharp = require("sharp");
 
 async function process() {
   try {
     await sharp("input.jpg")
          .tile({
       layout: "iiif",
-      id: "."
+      id: "data"
   })
       .toFile("data");
   } catch (error) {
     console.log(error);
   }
+  
+  // react-iiif-viewer chokes if it doesn have a 'sizes' key in the info file
+  
+  const infoFile = "./data/info.json"
+  const info = require(infoFile)
+  info.sizes = []
+  info["@id"] = "data"
+  
+  fs.writeFile(infoFile, JSON.stringify(info, null, 2), function err(){})
+  
 }
 
 process();
